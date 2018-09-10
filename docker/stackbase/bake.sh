701503bebb12f3d8b6144724724c688ca16625a1
Christine Banek <cbanek@lsst.org>
2018-09-10 15:15:35 -0700

    [DM-15396] Update branches to master, note failing test.
    There's some test failures that are blocking us, but for now just
    don't run the tests until we fix the associated ticket.

--- docker/stackbase/bake.sh
+++ docker/stackbase/bake.sh
@@ -11,17 +11,17 @@ source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 
 cd ~
-git clone https://github.com/lsst/dax_webservcommon.git -b tickets/DM-15396
+git clone https://github.com/lsst/dax_webservcommon.git -b master
 (cd dax_webservcommon && pip install -r requirements.txt && pip install . && python setup.py test)
 
-git clone https://github.com/lsst/dax_metaserv.git -b tickets/DM-15396
+git clone https://github.com/lsst/dax_metaserv.git -b master
 (cd dax_metaserv && pip install -r requirements.txt && pip install . && python setup.py test)
 
-git clone https://github.com/lsst/dax_dbserv.git -b tickets/DM-15396
+git clone https://github.com/lsst/dax_dbserv.git -b master
 (cd dax_dbserv && pip install -r requirements.txt && pip install . && python setup.py test)
 
-git clone https://github.com/lsst/dax_imgserv.git -b tickets/DM-15396
-# TODO: Still need to fix test.
+git clone https://github.com/lsst/dax_imgserv.git -b master
+# TODO(DM-15694): Still need to fix imgserv test.
 #(cd dax_imgserv && pip install -r requirements.txt && pip install . && python setup.py test)
 (cd dax_imgserv && pip install -r requirements.txt && pip install .)
 
