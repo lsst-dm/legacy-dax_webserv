9ff2cd1f0fee191b9b9f168bea71fa8e8b1bb735
Christine Banek <cbanek@lsst.org>
2019-01-07 17:21:15 -0600

    [DM-17059] Update to work on lsst-lsp-int.ncsa.illinois.edu
    Change the ingress rule and test for the hostname.

--- integration_test.sh
+++ integration_test.sh
@@ -2,7 +2,7 @@
 
 echo "Image serve tests..."
 
-curl --fail -o /tmp/calexp_i6_out.fits "https://lsst-pdac.ncsa.illinois.edu/api/image/v1/DC_W13_Stripe82?ds=calexp&ra=37.644598&dec=0.104625&filter=r"
+curl --fail -o /tmp/calexp_i6_out.fits "https://lsst-lsp-int.ncsa.illinois.edu/api/image/v1/DC_W13_Stripe82?ds=calexp&ra=37.644598&dec=0.104625&filter=r"
 
 echo "Meta serve tests..."
-curl --fail -o /tmp/table_metadata.json "https://lsst-pdac.ncsa.illinois.edu/api/meta/v1/db/1/1/tables/1/"
+curl --fail -o /tmp/table_metadata.json "https://lsst-lsp-int.ncsa.illinois.edu/api/meta/v1/db/1/1/tables/1/"
