2519a488f1a56f9bdf3e227a99443cfc3ed943df
Kenny Lo <kennylo@slac.stanford.edu>
2019-04-25 17:34:30 -0500

    Adjust to current way of accessing persistentvolumes

--- kube/stable/create.sh
+++ kube/stable/create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-stable'}
 
-kubectl create -f dax-webserv-datasets-volume.yaml
-kubectl create -f dax-webserv-datasets-claim.yaml --namespace $DAX_NAMESPACE
-kubectl create -f dax-webserv-deployment.yaml --namespace $DAX_NAMESPACE
+# kubectl create -f dax-webserv-datasets-volume.yaml
+# kubectl create -f dax-webserv-datasets-claim.yaml --namespace $DAX_NAMESPACE
+# kubectl create -f dax-webserv-deployment.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-webserv-service.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-webserv-ingress.yaml --namespace $DAX_NAMESPACE
--- kube/stable/destroy.sh
+++ kube/stable/destroy.sh
@@ -4,5 +4,5 @@ DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-stable'}
 kubectl delete ingress dax-webserv-ingress --namespace $DAX_NAMESPACE
 kubectl delete service dax-webserv-service --namespace $DAX_NAMESPACE
 kubectl delete deployment dax-webserv-deployment --namespace $DAX_NAMESPACE
-kubectl delete pvc dax-webserv-datasets-claim --namespace $DAX_NAMESPACE
-kubectl delete pv dax-prod-webserv-datasets-volume
+# kubectl delete pvc dax-webserv-datasets-claim --namespace $DAX_NAMESPACE
+# kubectl delete pv dax-prod-webserv-datasets-volume
